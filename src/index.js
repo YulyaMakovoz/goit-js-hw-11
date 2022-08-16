6df6728719cbd30bc6088b216bccad42a78c8d92
YulyaMakovoz <97025295+YulyaMakovoz@users.noreply.github.com>
2022-08-16 16:50:47 +0300

    Добавляет все функции задачи

--- src/index.js
+++ src/index.js
@@ -1,33 +1,40 @@
-// Добавить notiflix!!
+
 // Добавить оформление!
 
 const API_KEY = '29274974-75b7ad93639b97858ba53233b';
 const axios = require('axios');
 import Notiflix from 'notiflix';
+const debounce = require('lodash.debounce');
 
 const inputEl = document.querySelector('input');
 const galleryEl = document.querySelector('.gallery');
-const formEL = document.querySelector('.search-form')
+const formEL = document.querySelector('.search-form');
+const buttonLoadMore = document.querySelector('.load-more');
 
 let currentPage = 1;
+let perPage = 4;
 let item = inputEl.value;
 
 
+buttonLoadMore.classList.add('is-hidden')
+
 formEL.addEventListener('submit', onSubmit);
+buttonLoadMore.addEventListener('click', onLoadMore);
 
 // Делаем запрос пользователя с данным ID
 async function fetchItems() {
     try {
-    const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${item}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${currentPage}`)
+    const response = await axios.get(`https://pixabay.com/api/?key=${API_KEY}&q=${item}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${perPage}&page=${currentPage}`)
         const dataImages = await response.data.hits;
+        
 // Если бэкенд возвращает пустой массив, значит ничего подходящего найдено небыло. В таком случае показывай уведомление с текстом "Sorry, there are no images matching your search query. Please try again.".
         if (dataImages.length === 0) {
+            buttonLoadMore.classList.add('is-hidden')
             Notiflix.Notify.info("Sorry, there are no images matching your search query. Please try again.")
+            
         }
-//         return {dataImages,
-//     totalHits: response.data.totalHits,
-// }
-        return dataImages
+//       
+        return dataImages;
     }
             catch (error) {
             console.log(error);
@@ -41,22 +48,28 @@ function onSubmit(event) {
     
     // При поиске по новому ключевому слову необходимо полностью очищать содержимое галереи, чтобы не смешивать результаты.
     galleryEl.innerHTML = "";
+    buttonLoadMore.classList.add('is-hidden')
 
     item = inputEl.value;
     item;
-console.log(item);
-    fetchItems()
-        .then(images => { showResult(images); })
-        .catch(error => (console.log(error)))
+    console.log(item);
+    currentPage = 1;
+        fetchItems()
+        // .then(images => { if (dataImages.length === 0) {showResult(images);}  })
+        // .catch(error => (console.log(error)));
     
-    
+    .then(images => { showResult(images); })
+        .catch(error => (console.log(error)));
+    // setTimeout(() => {
+    //      buttonLoadMore.classList.remove('is-hidden');
+    // },300)
+    // buttonLoadMore.classList.remove('is-hidden');
 }
-
-
+    
 
 function createOneItem(image) {
     return `<div class="photo-card">
-  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+  <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width ="50" />
   <div class="info">
     <p class="info-item">
       <b>Likes ${image.likes}</b>
@@ -78,14 +91,38 @@ function showOneItem(dataImages) {
     return dataImages.reduce((acc,image) =>acc +createOneItem(image),"")
 }
 function showResult(dataImages) {
-galleryEl.insertAdjacentHTML('beforeend',showOneItem(dataImages))
+    galleryEl.insertAdjacentHTML('beforeend', showOneItem(dataImages))
+    // buttonLoadMore.classList.remove('is-hidden');
+    if (dataImages.length !== 0) {
+        setTimeout(() => {
+            buttonLoadMore.classList.remove('is-hidden');
+        }, 300)
+    }
+}
+
+function onLoadMore() {
+  
+    currentPage += 1;
+    item = inputEl.value;
+     fetchItems()
+        .then(images => { showResult(images); })
+         .catch(error => (console.log(error)))
+     onCheckPage();
+    // buttonLoadMore.classList.remove('is-hidden');
+
+    
+}
+    function onCheckPage() {
+    
+    buttonLoadMore.classList.add('is-hidden')
+   
+    Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
+
 }
 
 //  fetch(`https://pixabay.com/api/?key=${API_KEY}&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`)
 
 
-
-// Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")
 
 
 
